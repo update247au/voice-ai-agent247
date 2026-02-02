178a35b5ceaf54f0ec6564f11bbbfc0fa37db22d
update247au <58158743+update247au@users.noreply.github.com>
2026-02-02 20:11:40 +1100

    Update index.js

--- index.js
+++ index.js
@@ -109,7 +109,7 @@ fastify.register(async (fastify) => {
             openAiWs.send(JSON.stringify(sessionUpdate));
 
             // Uncomment the following line to have AI speak first:
-            // sendInitialConversationItem();
+            sendInitialConversationItem();
         };
 
         // Send initial conversation item if AI talks first
