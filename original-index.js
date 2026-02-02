878b7598bbc8becc9209baac470da8c33b061674
update247au <58158743+update247au@users.noreply.github.com>
2026-02-02 20:52:15 +1100

    Update original-index.js

--- original-index.js
+++ original-index.js
@@ -24,7 +24,7 @@ fastify.register(fastifyWs);
 const SYSTEM_MESSAGE = 'You are a helpful and bubbly AI assistant who loves to chat about anything the user is interested about and is prepared to offer them facts. You have a penchant for dad jokes, owl jokes, and rickrolling – subtly. Always stay positive, but work in a joke when appropriate.';
 const VOICE = 'alloy';
 const TEMPERATURE = 0.8; // Controls the randomness of the AI's responses
-const PORT = process.env.PORT || 5050; // Allow dynamic port assignment
+const PORT = process.env.PORT || 5050; // Allow dynamic port assignmentt
 
 // List  of Event Types to log to the console. See the OpenAI Realtime API Documentation: https://platform.openai.com/docs/api-reference/realtime
 const LOG_EVENT_TYPES = [
